dab9abd77b15d4e8d254891fb285048a04c6a633
Giedrius Mėčius <giedriusmecius@gmail.com>
2021-01-25 18:46:16 +0100

    10th challenge done

--- 10.loopstudios-landing-page-main/index.js
+++ 10.loopstudios-landing-page-main/index.js
@@ -4,6 +4,7 @@ const mainText = document.getElementById("main-text");
 
 function showMenu() {
   burger.addEventListener("click", () => {
+    burger.classList.toggle("close");
     menu.classList.toggle("active");
     mainText.classList.toggle("hidden");
   });
