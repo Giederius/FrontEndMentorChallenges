e138a75ff6381bc6c4e80d5d7e8f08880e7a33b7
Giedrius Mėčius <giedriusmecius@gmail.com>
2021-05-19 18:44:43 +0200

    uploading new projects

---
+++ 13.fylo-dark-theme-landing-page-master/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        backgroundMain: "hsl(218, 28%, 13%)",
+        cyan: "hsl(176, 68%, 64%)",
+        blue: "hsl(198, 60%, 50%)",
+        testimonialBg: "hsl(219, 30%, 18%)",
+        introBg: "hsl(217, 28%, 15%)",
+        footerBg: " hsl(216, 53%, 9%)",
+      },
+      fontFamily: {
+        raleway: ["Raleway, sans-serif"],
+        openSans: ["Open Sans, sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
