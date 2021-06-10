fe1f516aceeb8855558b67350640ea03a68a4463
Giedrius Mėčius <giedriusmecius@gmail.com>
2021-06-10 21:20:56 +0200

    finished mobile design, started with responsivness

--- 14.clipboard-landing-page-master/tailwind.config.js
+++ 14.clipboard-landing-page-master/tailwind.config.js
@@ -3,9 +3,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: (theme) => ({
-        mobileBg: "url('/images/bg-header-mobile.png')",
-      }),
+      backgroundImage: {
+        mobileBg:
+          "linear-gradient(to top, rgba(255,255,255), transparent), url('/images/bg-header-mobile.png')",
+      },
+
       fontFamily: {
         bai: ["Bai Jamjuree, sans-serif"],
       },
@@ -14,6 +16,7 @@ module.exports = {
         bodyText: "hsl(201, 11%, 66%)",
         primaryBtn: "hsl(171, 66%, 44%)",
         secondaryBtn: "hsl(233, 100%, 69%)",
+        footerColor: "rgb(243,244,246)",
       },
       boxShadow: {
         xl: "0 20px 25px -17px hsl(171, 66%, 44%)",
